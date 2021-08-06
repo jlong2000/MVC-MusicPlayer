6b30740c1b1b5a320c2b327041870f98abe3eb3b
Jiang Long <jlong20110323@gmail.com>
2021-08-06 05:05:21 -0400

    Setup project

---
+++ Music Player/Controller/ViewControllers/AlbumListViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AlbumListViewController.swift
+//  Music Player
+//
+//  Created by Jiang Long on 8/6/21.
+//
+
+import UIKit
+
+class AlbumListViewController: BaseViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+
+  /*
+   // MARK: - Navigation
+
+   // In a storyboard-based application, you will often want to do a little preparation before navigation
+   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+   // Get the new view controller using segue.destination.
+   // Pass the selected object to the new view controller.
+   }
+   */
+
+}
